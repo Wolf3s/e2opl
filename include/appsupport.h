d3972ad35a336ecdf557965d75576a01f136f44c
o0on <o0on@f7fb7b69-a7aa-4511-8a89-062b96478b4d>
2015-03-14 17:08:46 +0000

    cleanup

--- include/appsupport.h
+++
@@ -1,9 +0,0 @@
-#ifndef __APP_SUPPORT_H
-#define __APP_SUPPORT_H
-
-#include "include/iosupport.h"
-
-void appInit();
-item_list_t* appGetObject(int initOnly);
-
-#endif
--- modules/network/smap/xfer.h
+++
@@ -1,3 +0,0 @@
-inline int HandleRxIntr(struct SmapDriverData *SmapDrivPrivData);
-int SMAPSendPacket(const void *data, unsigned int length);
-
